2d5d25075f849babb28c62d7e57e353ec083e0c5
a-nst-y <82892365+a-nst-y@users.noreply.github.com>
2022-11-03 03:18:36 +0300

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,40 @@
+class CountVectorizer:
+    """Returns matrix of counts of words in texts
+    and all distinct words from all texts """
+    def __init__(self):
+        self.voc = []
+
+    def fit_transform(self, text: list) -> list:
+        """Returns the word frequency list"""
+        text_new = []
+        for element in text:
+            element = element.lower().split()
+            text_new.append(element)
+            for e in element:
+                if e not in self.voc:
+                    self.voc.append(e)
+        counts = []
+        for element in text_new:
+            single_text_count = []
+            for e in self.voc:
+                single_text_count.append(element.count(e))
+            counts.append(single_text_count)
+        return counts
+
+    def get_feature_names(self) -> list:
+        """Returns a list of unique words for all texts in the list"""
+        if len(self.voc) == 0:
+            print('No text!')
+        else:
+            return self.voc
+
+
+if __name__ == '__main__':
+    corpus = [
+     'Crock Pot Pasta Never boil pasta again',
+     'Pasta Pomodoro Fresh ingredients Parmesan to taste'
+    ]
+    vectorizer = CountVectorizer()
+    count_matrix = vectorizer.fit_transform(corpus)
+    print(count_matrix)
+    print(vectorizer.get_feature_names())
